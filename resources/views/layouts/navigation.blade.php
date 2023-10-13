5f7d25aef78b5732f9581a596e8fdd38b290f8ae
rym12 <ryuma@DESKTOP-LTG4VBB>
2023-10-14 00:37:25 +0900

    calendarナビ作成＊calendar画面には行けない

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('ホーム画面') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('profile.calendar')" :active="request()->routeIs('profile.calendar')">
+                        {{ __('カレンダー') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -69,6 +74,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('profile.calendar')" :active="request()->routeIs('profile.calendar')">
+                {{ __('Index') }}
             </x-responsive-nav-link>
         </div>
 
