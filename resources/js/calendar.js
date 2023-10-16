7f54424665694fed8dcee082067193a7d13865aa
rym12 <ryuma@DESKTOP-LTG4VBB>
2023-10-16 22:12:39 +0900

    calendarイベント表示機能

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -48,5 +48,23 @@ let calendar = new Calendar(calendarEl, {
                 });
         }
     },
+    events: function (info, successCallback, failureCallback) {
+        // Laravelのイベント取得処理の呼び出し
+        axios
+            .post("/schedule-get", {
+                start_date: info.start.valueOf(),
+                end_date: info.end.valueOf(),
+            })
+            .then((response) => {
+                // 追加したイベントを削除
+                calendar.removeAllEvents();
+                // カレンダーに読み込み
+                successCallback(response.data);
+            })
+            .catch(() => {
+                // バリデーションエラーなど
+                alert("登録を失敗しました");
+            });
+    },
 });
 calendar.render();
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ use App\Http\Controllers\ScheduleController;
 |
 */
 
+// イベント登録処理
 Route::post('/schedule-add', [ScheduleController::class, 'scheduleAdd'])->name('schedule-add');
+// イベント取得処理
+Route::post('/schedule-get', [ScheduleController::class, 'scheduleGet'])->name('schedule-get');
 
 Route::get('/calendar', function () {
     return view('calendar');
