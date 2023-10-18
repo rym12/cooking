1b236b35954735110391228901904c80b90c2aa5
ryogoooo <iidaryogo0526@gmail.com>
2023-10-18 23:46:40 +0900

    バグ対処

--- resources/views/create_family.blade.php
+++ resources/views/create_family.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <div class="container">
-    <form action="{{ route('family.send') }}" method="POST">
+    <form action="{{ route('family.create') }}" method="POST">
         @csrf
         <label for="family_id">Family id を入力してください</label>
         <input type="text" id="family_id" name="family_id" placeholder="入力してください">
