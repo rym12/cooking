7abc06a872d2eb571ad6964224aa4a2ef578db02
Haruki-Ichinose <haruichi0712@icloud.com>
2023-10-20 10:39:24 +0900

    chat2

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -1,41 +1,54 @@
+<!DOCTYPE html>
 <x-app-layout>
     <x-slot name="header">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </x-slot>
 
     <style>
-        .other::before {
-            content: "";
-            position: absolute;
-            top: 90%;
-            left: -15px;
-            margin-top: -30px;
-            border: 5px solid transparent;
-            border-right: 15px solid #c7deff;
+        /* メッセージコンテナのスタイル */
+        .message-container {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
         }
 
-        .self::after {
-            content: "";
-            position: absolute;
-            top: 50%;
-            left: 100%;
-            margin-top: -15px;
-            border: 3px solid transparent;
-            border-left: 9px solid #c7deff;
+        /* 基本的なメッセージのスタイル */
+        .message {
+            display: inline-block;
+            padding: 10px;
+            border-radius: 10px;
+            max-width: 70%;
+            clear: both;
+        }
+
+        /* ユーザー自身のメッセージのスタイル */
+        .self .message {
+            background-color: #c7deff;
+            align-self: flex-end;
+        }
+
+        /* 他のユーザーのメッセージのスタイル */
+        .other .message {
+            background-color: #f0f0f0;
         }
     </style>
 
+    <!-- 画面の全体的なレイアウトを設定 -->
     <body class="w-4/5 md:w-3/5 lg:w-2/5 m-auto">
+        <!-- メッセージ表示エリア -->
         <div class="my-4 p-4 rounded-lg bg-blue-200">
-            <ul>
+            <ul class="message-container">
                 @foreach ($chats as $chat)
-                    <li class="truncate chat-message {{ $chat->user_name === 'Haruki' ? 'my-message' : 'other-message' }}">
-                        <strong>{{ $chat->user_name }}</strong>: {{ $chat->message }}
+                    <li class="message {{ $chat->user_name === 'Haruki' ? 'self' : 'other' }}">
+                        <p class="text-xs">{{ $chat->created_at }} ＠{{ $chat->user_name }}</p>
+                        {{ $chat->message }}
                     </li>
                 @endforeach
             </ul>
         </div>
-        <form class="my-4 py-2 px-4 rounded-lg bg-gray-300 text-sm flex flex-col md:flex-row flex-grow" action="{{ route('chat.store') }}" method="POST">
+        
+        <!-- メッセージ送信フォーム -->
+        <form class="my-4 py-2 px-4 rounded-lg bg-gray-300 text-sm flex flex-col md:flex-row flex-grow" action="/chat" method="POST">
             @csrf
             <input type="hidden" name="user_identifier" value="test">
             <input class="py-1 px-2 rounded text-center flex-initial" type="text" name="user_name" placeholder="UserName" maxlength="20">
@@ -43,5 +56,4 @@
             <button class="mt-2 md:mt-0 md:ml-2 py-1 px-2 rounded text-center bg-gray-500 text-white" type="submit">Send</button>
         </form>
     </body>
-    
 </x-app-layout>
